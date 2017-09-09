2b17ad277ee59aa52194d2fa140b1a089de44c2b
Brent Vatne <brentvatne@gmail.com>
2017-09-09 11:38:04 -0700

    Update to SDK20

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Snap a photo or grab it from your camera roll, upload it. See `/backend`
 for an example node service to handle the upload to s3, and `/frontend`
-for the Exponent app.
+for the Expo app.
 
-Try it: https://getexponent.com/@community/image-upload-example
+Try it: https://expo.io/@community/image-upload-example
--- frontend/App.js
+++ frontend/App.js
@@ -11,25 +11,26 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import Exponent, {
-  Constants,
-  ImagePicker,
-  registerRootComponent,
-} from 'exponent';
+import Exponent, { Constants, ImagePicker, registerRootComponent } from 'expo';
 
-@registerRootComponent
 export default class App extends React.Component {
   state = {
     image: null,
     uploading: false,
-  }
+  };
 
   render() {
     let { image } = this.state;
 
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={{fontSize: 20, marginBottom: 20, textAlign: 'center', marginHorizontal: 15}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text
+          style={{
+            fontSize: 20,
+            marginBottom: 20,
+            textAlign: 'center',
+            marginHorizontal: 15,
+          }}>
           Example: Upload ImagePicker result
         </Text>
 
@@ -38,13 +39,10 @@ export default class App extends React.Component {
           title="Pick an image from camera roll"
         />
 
-        <Button
-          onPress={this._takePhoto}
-          title="Take a photo"
-        />
+        <Button onPress={this._takePhoto} title="Take a photo" />
 
-        { this._maybeRenderImage() }
-        { this._maybeRenderUploadingOverlay() }
+        {this._maybeRenderImage()}
+        {this._maybeRenderUploadingOverlay()}
 
         <StatusBar barStyle="default" />
       </View>
@@ -54,16 +52,20 @@ export default class App extends React.Component {
   _maybeRenderUploadingOverlay = () => {
     if (this.state.uploading) {
       return (
-        <View style={[StyleSheet.absoluteFill, {backgroundColor: 'rgba(0,0,0,0.4)', alignItems: 'center', justifyContent: 'center'}]}>
-          <ActivityIndicator
-            color="#fff"
-            animating
-            size="large"
-          />
+        <View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: 'rgba(0,0,0,0.4)',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+          ]}>
+          <ActivityIndicator color="#fff" animating size="large" />
         </View>
       );
     }
-  }
+  };
 
   _maybeRenderImage = () => {
     let { image } = this.state;
@@ -72,32 +74,35 @@ export default class App extends React.Component {
     }
 
     return (
-      <View style={{
-        marginTop: 30,
-        width: 250,
-        borderRadius: 3,
-        elevation: 2,
-        shadowColor: 'rgba(0,0,0,1)',
-        shadowOpacity: 0.2,
-        shadowOffset: {width: 4, height: 4},
-        shadowRadius: 5,
-      }}>
-        <View style={{borderTopRightRadius: 3, borderTopLeftRadius: 3, overflow: 'hidden'}}>
-          <Image
-            source={{uri: image}}
-            style={{width: 250, height: 250}}
-          />
+      <View
+        style={{
+          marginTop: 30,
+          width: 250,
+          borderRadius: 3,
+          elevation: 2,
+          shadowColor: 'rgba(0,0,0,1)',
+          shadowOpacity: 0.2,
+          shadowOffset: { width: 4, height: 4 },
+          shadowRadius: 5,
+        }}>
+        <View
+          style={{
+            borderTopRightRadius: 3,
+            borderTopLeftRadius: 3,
+            overflow: 'hidden',
+          }}>
+          <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
         </View>
 
         <Text
           onPress={this._copyToClipboard}
           onLongPress={this._share}
-          style={{paddingVertical: 10, paddingHorizontal: 10}}>
+          style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
           {image}
         </Text>
       </View>
     );
-  }
+  };
 
   _share = () => {
     Share.share({
@@ -105,51 +110,51 @@ export default class App extends React.Component {
       title: 'Check out this photo',
       url: this.state.image,
     });
-  }
+  };
 
   _copyToClipboard = () => {
     Clipboard.setString(this.state.image);
     alert('Copied image URL to clipboard');
-  }
+  };
 
   _takePhoto = async () => {
     let pickerResult = await ImagePicker.launchCameraAsync({
       allowsEditing: true,
-      aspect: [4,3]
+      aspect: [4, 3],
     });
 
     this._handleImagePicked(pickerResult);
-  }
+  };
 
   _pickImage = async () => {
     let pickerResult = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
-      aspect: [4,3]
+      aspect: [4, 3],
     });
 
     this._handleImagePicked(pickerResult);
-  }
+  };
 
-  _handleImagePicked = async (pickerResult) => {
+  _handleImagePicked = async pickerResult => {
     let uploadResponse, uploadResult;
 
     try {
-      this.setState({uploading: true});
+      this.setState({ uploading: true });
 
       if (!pickerResult.cancelled) {
         uploadResponse = await uploadImageAsync(pickerResult.uri);
         uploadResult = await uploadResponse.json();
-        this.setState({image: uploadResult.location});
+        this.setState({ image: uploadResult.location });
       }
-    } catch(e) {
-      console.log({uploadResponse});
-      console.log({uploadResult});
-      console.log({e});
+    } catch (e) {
+      console.log({ uploadResponse });
+      console.log({ uploadResult });
+      console.log({ e });
       alert('Upload failed, sorry :(');
     } finally {
-      this.setState({uploading: false});
+      this.setState({ uploading: false });
     }
-  }
+  };
 }
 
 async function uploadImageAsync(uri) {
@@ -178,7 +183,7 @@ async function uploadImageAsync(uri) {
     method: 'POST',
     body: formData,
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'multipart/form-data',
     },
   };
