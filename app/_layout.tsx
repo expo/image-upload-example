a7b7667a423edd9dd638abff8a51cd6219483c66
evanbacon <bacon@expo.io>
2026-05-24 19:47:06 -0700

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from "@react-navigation/native";
+} from "expo-router/react-navigation";
 
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
